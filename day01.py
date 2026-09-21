d0cc48a80baacf75ab3d148d6a31487c92874cac
tharuno7 <tharunerothu@gmail.com>
2026-09-21 15:24:14 +0530

    Update day01.py

--- day01.py
+++ day01.py
@@ -1,52 +1,52 @@
 # #identity tokens and statement
-# a = 45
-# b = 35
-# c = 20
-# print(a + b + c) #output: 100
+a = 45
+b = 35
+c = 20
+print(a + b + c) #output: 100
 
 # #keywords
-# import keyword 
-# print(keyword.kwlist) #output:['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
-# print(len(keyword.kwlist)) #output: 35
+import keyword 
+print(keyword.kwlist) #output:['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+print(len(keyword.kwlist)) #output: 35
 
 #Identifiers
-# num1 = 10 
-# n2m = 20 
-# n_m = 30
-# _num4 = 40
-# 3num = 30 
-# n@m = 40 
-# for = 30
-# a = 30 
-# A = 20
-# print(a) #output: 30
-# print(A) #output: 20
+num1 = 10 
+n2m = 20 
+n_m = 30
+_num4 = 40
+3num = 30 
+n@m = 40 
+for = 30
+a = 30 
+A = 20
+print(a) #output: 30
+print(A) #output: 20
 
 #variables 
-# name = 'rakesh'
-# age = 23
+name = 'rakesh'
+age = 23
 
 # #assignment 
-# a = 40 
-# b = 50
+a = 40 
+b = 50
 
 # multiple assignment
-# a, b, c = 10, 20, 30
-# print(a, b, c)  # output: 10 20 30
-# a = b = c = 10
-# print(a, b, c)  # output: 10 10 10
+a, b, c = 10, 20, 30
+print(a, b, c)  # output: 10 20 30
+a = b = c = 10
+print(a, b, c)  # output: 10 10 10
 
 # #reassignment
-# z = 10
-# z = 20 
-# z = 30
-# print(z) #output: 30
+z = 10
+z = 20 
+z = 30
+print(z) #output: 30
 
 #deleting variable
 a = [1,2,3]
 b = a 
 del a 
-#print(a) #output: NameError
+print(a) #output: NameError
 print(b) #output: [1,2,3]
 
 #swapping variables
@@ -65,20 +65,20 @@ a = a - b
 print(a, b) #output: 20 10
 
 # #using * and /
-# a = 10
-# b = 20
-# a = a * b 
-# b = a / b 
-# a = a / b
-# print(a, b)
+a = 10
+b = 20
+a = a * b 
+b = a / b 
+a = a / b
+print(a, b)
 
 # #using ^. A^A = 0, A^0 = A. (A^B)^C  = A^(B^C) 
-# a = 10  #^ cannot be used for float
-# b = 20
-# a = a ^ b 
-# b = a ^ b  #(a^b) ^ b = a ^ b ^ b = a ^ 0 = a
-# a = a ^ b  # (a^b) ^ a = a ^ b ^ a = b
-# print(a, b)
+a = 10  #^ cannot be used for float
+b = 20
+a = a ^ b 
+b = a ^ b  #(a^b) ^ b = a ^ b ^ b = a ^ 0 = a
+a = a ^ b  # (a^b) ^ a = a ^ b ^ a = b
+print(a, b)
 
 # #single line comment
 # '''multi
