7d4e757ae52027fb471919c661858af158b0af73
tharuno7 <tharunerothu@gmial.com>
2026-09-23 00:41:39 +0530

    day02.py

--- day01.py
+++ day01.py
@@ -1,15 +1,16 @@
-# #identity tokens and statement
+
+#identity tokens and statement
 a = 45
 b = 35
 c = 20
 print(a + b + c) #output: 100
 
-# #keywords
+#keywords
 import keyword 
 print(keyword.kwlist) #output:['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
 print(len(keyword.kwlist)) #output: 35
 
-#Identifiers
+# Identifiers
 num1 = 10 
 n2m = 20 
 n_m = 30
@@ -26,7 +27,11 @@ print(A) #output: 20
 name = 'rakesh'
 age = 23
 
-# #assignment 
+# variables 
+name = 'rakesh'
+age = 23
+
+#assignment 
 a = 40 
 b = 50
 
@@ -36,52 +41,52 @@ print(a, b, c)  # output: 10 20 30
 a = b = c = 10
 print(a, b, c)  # output: 10 10 10
 
-# #reassignment
+#reassignment
 z = 10
 z = 20 
 z = 30
 print(z) #output: 30
 
-#deleting variable
+# deleting variable
 a = [1,2,3]
 b = a 
 del a 
 print(a) #output: NameError
 print(b) #output: [1,2,3]
 
-#swapping variables
+# swapping variables
 a = 10
 b = 20 
-a,b = b,a 
+a,b = b,a  
 print(a, b)  #output: 20 10
 
 # without third variable
 # using + and - 
 a = 10
 b = 20
-a = a + b 
+a = a + b  
 b = a - b 
 a = a - b 
 print(a, b) #output: 20 10
 
-# #using * and /
+#using * and /
 a = 10
 b = 20
 a = a * b 
 b = a / b 
 a = a / b
-print(a, b)
+print(a, b) #output: 20 10 
 
-# #using ^. A^A = 0, A^0 = A. (A^B)^C  = A^(B^C) 
+#using ^. A^A = 0, A^0 = A. (A^B)^C  = A^(B^C) 
 a = 10  #^ cannot be used for float
 b = 20
 a = a ^ b 
 b = a ^ b  #(a^b) ^ b = a ^ b ^ b = a ^ 0 = a
 a = a ^ b  # (a^b) ^ a = a ^ b ^ a = b
-print(a, b)
+print(a, b) #output: 20 10
 
-# #single line comment
-# '''multi
-# line
-# comment
-# '''
+#single line comment
+'''multi
+line
+comment
+'''
