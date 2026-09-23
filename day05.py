52f22a6790c786bb70786c1c1461431c62835a6f
tharuno7 <tharunerothu@gmial.com>
2026-09-23 15:19:59 +0530

    day05.py

--- day05.py
+++ day05.py
@@ -1,96 +1,96 @@
-# # #append()
-# l = ['a', 'b', 'c']
-# l.append(34)
-# l.append(34.3)
-# l.append(4+3j)
-# l.append(True)
-# l.append(None)
-# l.append([0,1,2])
-# l.append((3,4,5))
-# l.append({6,7,8})
-# l.append({9:'a', 10:'b', 11:'c'})
-# l.append('rakesh')
-# l.append(range(12,15))
-# print(l)        # ['a', 'b', 'c', 34, 34.3, (4+3j), True, None, [0, 1, 2], (3, 4, 5), {8, 6, 7}, {9: 'a', 10: 'b', 11: 'c'}, 'rakesh', range(12, 15)]
+# #append()
+l = ['a', 'b', 'c']
+l.append(34)
+l.append(34.3)
+l.append(4+3j)
+l.append(True)
+l.append(None)
+l.append([0,1,2])
+l.append((3,4,5))
+l.append({6,7,8})
+l.append({9:'a', 10:'b', 11:'c'})
+l.append('rakesh')
+l.append(range(12,15))
+print(l)        # ['a', 'b', 'c', 34, 34.3, (4+3j), True, None, [0, 1, 2], (3, 4, 5), {8, 6, 7}, {9: 'a', 10: 'b', 11: 'c'}, 'rakesh', range(12, 15)]
 
-# # extend() 
-# l = ['a', 'b', 'c']
-# l.extend(34)          #TypeError: 'int' object is not iterable
-# l.extend(34.3)        #TypeError: 'float' object is not iterable
-# l.extend(4+3j)        #TypeError: 'complex' object is not iterable
-# l.extend(True)        #TypeError: 'bool' object is not iterable
-# l.extend(None)        #TypeError: 'None' object is not iterable
-# l.extend([0,1,2])
-# l.extend((3,4,5))
-# l.extend({6,7,8})
-# l.extend({9:'a', 10:'b', 11:'c'})
-# l.extend('rakesh')
-# l.extend(range(12,15))
-# print(l)               #  ['a', 'b', 'c', 0, 1, 2, 3, 4, 5, 8, 6, 7, 9, 10, 11, 'r', 'a', 'k', 'e', 's', 'h', 12, 13, 14]
+# extend() 
+l = ['a', 'b', 'c']
+l.extend(34)          #TypeError: 'int' object is not iterable
+l.extend(34.3)        #TypeError: 'float' object is not iterable
+l.extend(4+3j)        #TypeError: 'complex' object is not iterable
+l.extend(True)        #TypeError: 'bool' object is not iterable
+l.extend(None)        #TypeError: 'None' object is not iterable
+l.extend([0,1,2])
+l.extend((3,4,5))
+l.extend({6,7,8})
+l.extend({9:'a', 10:'b', 11:'c'})
+l.extend('rakesh')
+l.extend(range(12,15))
+print(l)               #  ['a', 'b', 'c', 0, 1, 2, 3, 4, 5, 8, 6, 7, 9, 10, 11, 'r', 'a', 'k', 'e', 's', 'h', 12, 13, 14]
 
-# # insert() 
-# #positive index
-# l = ['a', 'b', 'c', 'd']
-# l.insert(2, 'hi')
-# print(l)                  #['a', 'b', 'hi', 'c', 'd']
-# l.insert(10, 'hi')
-# print(l)                  #['a', 'b', 'hi', 'c', 'd','hi]
-# #negative index
-# l = ['a', 'b', 'c', 'd', 'e']
-# l.insert(-2, 'hi')        #['a', 'b', 'c', 'hi', 'd', 'e']
-# print(l)
-# l.insert(-100, 'hi')
-# print(l)                  #['hi', 'a', 'b', 'c', 'hi', 'd', 'e']
+# insert() 
+#positive index
+l = ['a', 'b', 'c', 'd']
+l.insert(2, 'hi')
+print(l)                  #['a', 'b', 'hi', 'c', 'd']
+l.insert(10, 'hi')
+print(l)                  #['a', 'b', 'hi', 'c', 'd','hi]
+#negative index
+l = ['a', 'b', 'c', 'd', 'e']
+l.insert(-2, 'hi')        #['a', 'b', 'c', 'hi', 'd', 'e']
+print(l)
+l.insert(-100, 'hi')
+print(l)                  #['hi', 'a', 'b', 'c', 'hi', 'd', 'e']
 
-# #pop()
-# l = [1, 2, 3, 4, 5]
-# a = l.pop() 
-# print(a, l)          # 5 [1, 2, 3, 4]
-# b = l.pop(2)
-# print(b, l)          # 3 [1, 2, 4]
-# # c = l.pop(7)       # IndexError: pop index out of range
-# del l[0]  
-# print(l)             # [2,4]
+#pop()
+l = [1, 2, 3, 4, 5]
+a = l.pop() 
+print(a, l)          # 5 [1, 2, 3, 4]
+b = l.pop(2)
+print(b, l)          # 3 [1, 2, 4]
+# c = l.pop(7)       # IndexError: pop index out of range
+del l[0]  
+print(l)             # [2,4]
 
-# # remove()
-# l = [1, 2, 3, 4]
-# a  = l.remove(3)
-# print(a, l)          # None [1, 2, 4]
-# print(l.remove(5))   # ValueError: list.remove(x): x not in list
+# remove()
+l = [1, 2, 3, 4]
+a  = l.remove(3)
+print(a, l)          # None [1, 2, 4]
+print(l.remove(5))   # ValueError: list.remove(x): x not in list
 
-# # clear()  
-# l = [1, 2, 3, 4, 5]
-# l.clear()
-# print(l)   # []
+# clear()  
+l = [1, 2, 3, 4, 5]
+l.clear()
+print(l)   # []
 
-# # reverse() 
-# l = [1, 2, 3, 4, 5]
-# print(id(l))             # 3137845901504
-# a = l.reverse()          
-# print(a, l)              # None [5, 4, 3, 2, 1]
-# print(id(l))             # 3137845901504
+# reverse() 
+l = [1, 2, 3, 4, 5]
+print(id(l))             # 3137845901504
+a = l.reverse()          
+print(a, l)              # None [5, 4, 3, 2, 1]
+print(id(l))             # 3137845901504
 
-# # sort()  
-# l = [1,4,2,6,5,3]
-# print(id(l))          # 1337295157440
-# a = l.sort()          
-# print(a, l)           # None [1, 2, 3, 4, 5, 6]
-# print(id(l))          # 1337295157440
-# l = [50,10,40,20,30]
-# print(l.sort(reverse=True)) # None
-# print(l)                    #[50, 40, 30, 20, 10] 
+# sort()  
+l = [1,4,2,6,5,3]
+print(id(l))          # 1337295157440
+a = l.sort()          
+print(a, l)           # None [1, 2, 3, 4, 5, 6]
+print(id(l))          # 1337295157440
+l = [50,10,40,20,30]
+print(l.sort(reverse=True)) # None
+print(l)                    #[50, 40, 30, 20, 10] 
 
 
-# # index() 
-# l = [1, 2, 1, 4, 6, 1, 7]
-# print(l.index(1))              # 0
-# print(l.index(1, 3))           # 5
-# # print(l.index(1, 3, 5))      # ValueError: list.index(x): x not in list
-# print(l.index(9))              # ValueError: list.index(x): x not in list
+# index() 
+l = [1, 2, 1, 4, 6, 1, 7]
+print(l.index(1))              # 0
+print(l.index(1, 3))           # 5
+# print(l.index(1, 3, 5))      # ValueError: list.index(x): x not in list
+print(l.index(9))              # ValueError: list.index(x): x not in list
 
-# # count() 
-# l = [1, 2, 1, 4, 1, 6, 7, 1]
-# print(l.count(1))               # 4
-# print(l.count(9))               # 0
+# count() 
+l = [1, 2, 1, 4, 1, 6, 7, 1]
+print(l.count(1))               # 4
+print(l.count(9))               # 0
 
 
