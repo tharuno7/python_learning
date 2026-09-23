2023baf47bcc0372fb797925c3b346bb798ad103
tharuno7 <tharunerothu@gmail.com>
2026-09-23 14:29:17 +0530

    Update day04.py

--- day04.py
+++ day04.py
@@ -3,16 +3,16 @@ a = '   python   '
 b = a.strip()       
 c = a.lstrip()
 d = a.rstrip()
-print(a, len(a))  
-print(b, len(b))   
-print(c, len(c))  
-print(d, len(d))   
+print(a, len(a))     # '   python   ' 14
+print(b, len(b))     # 'python' 6
+print(c, len(c))     # 'python   ' 9
+print(d, len(d))     # '   python' 9
 
 # #replace
 a = 'python is simple, python is easy to learn, python is all rounder'
 b = a.replace('python', 'java')
-print(a)  
-print(b)  
+print(a)    # 'python is simple, python is easy to learn, python is all rounder'
+print(b)    # 'java is simple, java is easy to learn, java is all rounder'
 
 # #upper, lower, swapcase, title, capitalize
 a = 'PYTHON is simple, PYTHON is easy to LEARN'
@@ -21,125 +21,100 @@ c = a.upper()
 d = a.swapcase()
 e = a.title()       
 f = a.capitalize()   
-print('original', a)       
-print('lower:', b)         
-print('upper:', c)         
-print('swapcase:', d)      
-print('title:', e)         
-print('capitalize:', f)    
+print('original', a)         # 'original' PYTHON is simple, PYTHON is easy to LEARN
+print('lower:', b)           # 'lower:' python is simple, python is easy to learn
+print('upper:', c)           # 'upper:' PYTHON IS SIMPLE, PYTHON IS EASY TO LEARN
+print('swapcase:', d)        # 'swapcase:' python IS SIMPLE, python IS easy TO learn
+print('title:', e)           # 'title:' Python Is Simple, Python Is Easy To Learn
+print('capitalize:', f)      # 'capitalize:' Python is simple, python is easy to learn
 
 #count, startswith, endswith
 s = 'python is python'
-print(s.count('th'))      
-print(s.startswith('py'))   
-print(s.endswith('onn'))   
+print(s.count('th'))          # 2
+print(s.startswith('py'))     # True
+print(s.endswith('onn'))      # False
  
 #find, index: 
 #    0123456789 
 s = 'abdcdefdgh'
-print(s.find('d'))          
-print(s.find('d', 5))       
-print(s.find('d', 5, 7))   
-print(s.index('d'))        
-print(s.index('d', 5))     
-print(s.index('d', 5, 7))   
-print()
-print()
+print(s.find('d'))            # 2
+print(s.find('d', 5))         # 4
+print(s.find('d', 5, 7))      # -1
+print(s.index('d'))           # 2
+print(s.index('d', 5))        # 4
+print(s.index('d', 5, 7))     # ValueError
+print()  # 
+print()  # 
 
 #rfind, rindex
 #    0123456789 
 s = 'abdcdefdgh'
-print(s.rfind('d'))          
-print(s.rfind('d', 5))       
-print(s.rfind('d', 5, 7))   
-print(s.rindex('d'))        
-print(s.rindex('d', 5))     
-print(s.rindex('d', 5, 7))   
-print()   
+print(s.rfind('d'))            # 7
+print(s.rfind('d', 5))         # 7
+print(s.rfind('d', 5, 7))      # 7
+print(s.rindex('d'))           # 7
+print(s.rindex('d', 5))        # 7
+print(s.rindex('d', 5, 7))     # 7
+print()     # 
  
 
 #isalpha: 
 a = 'aBcD'
 b = 'abc1'
 c = ''
-print(a.isalpha())  
-print(b.isalpha()) 
-print(c.isalpha())  
-print() 
-print()
+print(a.isalpha())    # True
+print(b.isalpha())    # False
+print(c.isalpha())    # False
+print()   # 
+print()  # 
 
 #isdigit
 a = '123'
 b = '12.3'
 c = '-123'
-print(a.isdigit()) 
-print(b.isdigit())  
-print(c.isdigit()) 
-print() 
-print()
+print(a.isdigit())   # True
+print(b.isdigit())   # False
+print(c.isdigit())   # False
+print()   # 
+print()  # 
 
 #isalnum: 
 a = 'Abc123'
 b = 'Abc@123'
 c = ' '
-print(a.isalnum())  
-print(b.isalnum())  
-print(c.isalnum())  
-print()
-print()
+print(a.isalnum())    # True
+print(b.isalnum())    # False
+print(c.isalnum())    # False
+print()  # 
+print()  # 
 
 #isupper: 
 a = 'ABC@123'
 b = '123'
 c = 'ABC123a'
-print(a.isupper()) 
-print(b.isupper())  
-print(c.isupper())  
-print()
-print()
+print(a.isupper())    # True
+print(b.isupper())    # False
+print(c.isupper())    # False
+print()  # 
+print()  # 
 
 #islower: 
 a = 'abc@123'
 b = '123'
 c = 'abc123A'
-print(a.islower()) 
-print(b.islower())  
-print(c.islower()) 
+print(a.islower())   # True
+print(b.islower())   # False
+print(c.islower())   # False
 
 #split
 s = 'abaca'
-print(s.split('a'))   
+print(s.split('a'))     # ['', 'b', 'c', '']
 s = '   '
-print(s.split(' '))  
-print(s.split())    
+print(s.split(' '))     # ['', '', '', '']
+print(s.split())        # []
   
 # #join
 a = [1,2,3,4]
 b = ['1', '2', '3']
-print('@'.join(a))  
-print('@'.join(b))   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print('@'.join(a))     # TypeError
+print('@'.join(b))     # 1@2@3
